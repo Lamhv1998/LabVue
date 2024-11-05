e08fb01b4a88cd06ce00d33082cca498bc4325f8
devlam <havulam1998z@gmail.com>
2024-11-05 09:01:28 +0700

    remote active

--- lab2_havulam_ps37973/src/components/RegistrantForm.vue
+++ lab2_havulam_ps37973/src/components/RegistrantForm.vue
@@ -5,7 +5,7 @@
     <ul class="nav nav-tabs">
       <li class="nav-item">
         <a
-          class="nav-link"
+          class="nav-link-active"
           aria-current="page"
           href="#"
           @click="handleChangeButton('signup')"
